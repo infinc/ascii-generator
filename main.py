fb8091260ef8cc16b29841c6c00d7425f7adec88
MARK11 <mark11@emuearuke11noMacBook-Air.local>
2026-08-08 15:15:43 +0900

    20260808-3

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import chat
-import properties
-from properties import cv2
-from properties import mimetypes
-from properties import sys
+import chat_factor
+import fanctions
+from fanctions import cv2
+from fanctions import mimetypes
+from fanctions import sys
 
 
-match properties.user_choice():
+match fanctions.user_choice():
     case 1:
-        path = properties.gain_path("jpg jpeg png webp")
+        path = fanctions.gain_path("jpg jpeg png webp", "sample.jpg")
         mime_type, _ = mimetypes.guess_type(path)
         if mime_type and mime_type.startswith('image'):
             img = cv2.imread(path)
@@ -15,12 +15,15 @@ match properties.user_choice():
             rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             height, width = gray.shape
             print("この画像の比率は(横:縦)" + str(width) + ":" + str(height))
-            width, height = properties.change_size_function(width, height)
-            properties.image_to_ascii_gray(gray, width, height)
+            width, height = fanctions.change_size_function(width, height)
+            fanctions.image_to_ascii_gray(gray, width, height)
             print("生成成功")
+        else:
+            print("エラー: 未対応のファイル、または拡張子の可能性があります。")
+            sys.exit()
 
     case 2:
-        path = properties.gain_path("jpg jpeg png webp")
+        path = fanctions.gain_path("jpg jpeg png webp", "sample.jpg")
         mime_type, _ = mimetypes.guess_type(path)
         if mime_type and mime_type.startswith('image'):
             img = cv2.imread(path)
@@ -28,13 +31,16 @@ match properties.user_choice():
             rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             height, width = gray.shape
             print("この画像の比率は(横:縦)" + str(width) + ":" + str(height))
-            width, height = properties.change_size_function(width, height)
+            width, height = fanctions.change_size_function(width, height)
             terminal = input("True Color(iTerm2,Pycharm,等)もしくは(Terminal,等)どちらを使いますか?(デフォルト=r)[t/r]: ")
-            properties.image_to_ascii_rgb(gray, rgb, width, height, terminal)
+            fanctions.image_to_ascii_rgb(gray, rgb, width, height, terminal)
             print("生成成功")
+        else:
+            print("エラー: 未対応のファイル、または拡張子の可能性があります。")
+            sys.exit()
 
     case 3:
-        path = properties.gain_path("gif")
+        path = fanctions.gain_path("gif", "")
         mime_type, _ = mimetypes.guess_type(path)
         if mime_type and (mime_type.startswith('video') or 'gif' in mime_type):
             cap = cv2.VideoCapture(path)
@@ -48,7 +54,7 @@ match properties.user_choice():
             fps = cap.get(cv2.CAP_PROP_FPS)
             print("この画像の比率は(横:縦)" + str(width) + ": " + str(height))
             print("FPS(フレームレート): " + str(int(fps)))
-            width, height = properties.change_size_function(width, height)
+            width, height = fanctions.change_size_function(width, height)
             try:
                 skip_frames = int(input("何フレームスキップしますか?(normal=1 or 2): "))
             except ValueError:
@@ -56,27 +62,34 @@ match properties.user_choice():
                 print("生成中止")
                 sys.exit()
             sleep_time = (1.0 / fps) * skip_frames if fps > 0 else 0.05
-            properties.gif_to_ascii_gray(width, height, cap, sleep_time)
+            fanctions.gif_to_ascii_gray(width, height, cap, sleep_time)
             print("生成成功")
+        else:
+            print("エラー: 未対応のファイル、または拡張子の可能性があります。")
+            sys.exit()
 
     case 4:
-        path = properties.gain_path("txt")
+        path = fanctions.gain_path("txt", "sample.txt")
         mime_type, _ = mimetypes.guess_type(path)
         if mime_type == 'text/plain':
-            properties.ascii_chars_list()
+            fanctions.ascii_chars_list()
             print("画像に変換するには、生成に使用された文字の種類を選ぶ必要があります")
             choose = input("文字を選んでください: ")
             if choose == "1":
-                selected_chars = properties.ASCII_CHARS_NORMAL
+                selected_chars = fanctions.ASCII_CHARS_NORMAL
             elif choose == "2":
-                selected_chars = properties.ASCII_CHARS_BLOCK
+                selected_chars = fanctions.ASCII_CHARS_BLOCK
             elif choose == "3":
-                selected_chars = properties.ASCII_CHARS_IMPACT
+                selected_chars = fanctions.ASCII_CHARS_IMPACT
             else:
                 print("エラー: 1、2、又は3を選んでください。")
                 print("生成中止")
                 sys.exit()
-            properties.ascii_to_image(path, selected_chars)
+            fanctions.ascii_to_image(path, selected_chars)
+
+        else:
+            print("エラー: 未対応のファイル、または拡張子の可能性があります。")
+            sys.exit()
 
     case 5:
         chat.start()
